974b09023dbe0bd5b88fc4c295bae7181006665f
Benjamin Knight <b-knight@users.noreply.github.com>
2019-08-05 21:10:06 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='StatExplore',
+      version='0.1',
+      description='A package to assist with statistical research',
+      url='http://github.com/b-knight/StatExplore',
+      author='Benjamin S. Knight',
+      author_email='knight.benjamin@gmail.com',
+      packages=['StatExplore'],
+      zip_safe=False)
